b951348411cf5ff3000bb00fe642ac8ac872c111
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2024-11-14 02:53:51 +0100

    device: linux: move ioctl code to main device source

--- device_linux.go
+++ device_linux.go
@@ -5,8 +5,11 @@
 package usbhid
 
 import (
+	"fmt"
+	"math"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
@@ -15,6 +18,82 @@ import (
 
 type deviceExtra struct {
 	file *os.File
+}
+
+var (
+	iocWrite    byte
+	iocRead     byte
+	iocSizeBits byte
+	iocDirBits  byte
+
+	iocNrShift   byte
+	iocTypeShift byte
+	iocSizeShift byte
+	iocDirShift  byte
+)
+
+func init() {
+	switch runtime.GOARCH {
+	case "386":
+		fallthrough
+	case "amd64":
+		fallthrough
+	case "arm":
+		fallthrough
+	case "arm64":
+		fallthrough
+	case "loong64":
+		fallthrough
+	case "riscv64":
+		fallthrough
+	case "s390x":
+		iocWrite = 1
+		iocRead = 2
+		iocSizeBits = 14
+		iocDirBits = 2
+
+	case "mips":
+		fallthrough
+	case "mips64":
+		fallthrough
+	case "mips64le":
+		fallthrough
+	case "mipsle":
+		fallthrough
+	case "ppc":
+		fallthrough
+	case "ppc64":
+		fallthrough
+	case "ppc64le":
+		fallthrough
+	case "sparc64":
+		iocWrite = 4
+		iocRead = 2
+		iocSizeBits = 13
+		iocDirBits = 3
+
+	default:
+		panic("usbhid: unsupported architecture")
+	}
+
+	iocNrShift = 0
+	iocTypeShift = 8
+	iocSizeShift = 16
+	iocDirShift = 16 + iocSizeBits
+}
+
+func ioc(dir byte, typ byte, nr byte, size uint16) uint32 {
+	dir = dir & (byte(math.Pow(2, float64(iocDirBits))) - 1)
+	size = size & (uint16(math.Pow(2, float64(iocSizeBits))) - 1)
+	return uint32(dir)<<iocDirShift | uint32(typ)<<iocTypeShift | uint32(nr)<<iocNrShift | uint32(size)<<iocSizeShift
+}
+
+func ioctl(fd uintptr, request uint, arg uintptr) (int, error) {
+	rv, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(request), arg)
+	if errno != 0 {
+		return 0, fmt.Errorf("ioctl failed: 0x%x: %s", request, errno)
+	}
+	return int(rv), nil
 }
 
 func sysfsReadAsBytes(dir string, entry string) ([]byte, error) {
